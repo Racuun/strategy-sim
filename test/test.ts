612f2c5cec2ccd77313772beaa8de1154a920ba5
Racuun <lukaszkmiecikdodtkowe@gmail.com>
2022-11-29 23:38:36 +0100

    test: :white_check_mark: run simple test for remote database

--- test/test.ts
+++ test/test.ts
@@ -1,17 +1,27 @@
 import StrategySim from '../index'
 
-const Simulator = new StrategySim.Simulator(10000, StrategySim.Source.SPY_M1);
 
-let i=0;
-function testFunction(Close:number, Balance:number, Buy: ()=>any, Sell:(index:number)=>any) {
-    if(Balance - Close > 1000)
-        Buy()
-    else {
-        Sell(i)
-        i++;
+
+async function main(): Promise<void> {
+
+
+    const Simulator = await StrategySim.Simulator.build(10000, StrategySim.Source.MSFT_EOD);
+
+    let i=0;
+    function testFunction(Close:number, Balance:number, Buy: ()=>any, Sell:(index:number)=>any) {
+        if(Balance - Close > 1000)
+            Buy()
+        else {
+            Sell(i)
+            i++;
+        }
     }
-}
 
-Simulator.provideLogic(({Close, Balance, Buy, Sell}) => testFunction(Close, Balance, Buy, Sell))
+    Simulator.provideLogic(({Close, Balance, Buy, Sell}) => testFunction(Close, Balance, Buy, Sell))
 
-Simulator.run()
+    Simulator.run()
+
+    
+  }
+  
+  main().catch(console.error);
